56f04daf93183c34979deb63510305485a01fd1c
mnewman12 <mnewman128@yahoo.com>
2021-08-02 14:16:15 -0400

    vue app

---
+++ discover-vue/src/components/Discover.vue
@@ -0,0 +1,532 @@
+<template>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="https://glitch.com/favicon.ico" />
+
+    <!-- Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Meta tags for SEO and social sharing -->
+    <link rel="canonical" href="https://glitch-hello-website.glitch.me/" />
+    <meta
+    name="description"
+    content="A simple website, built with Glitch. Remix it to get your own."
+    />
+    
+    <meta name="robots" content="index,follow" />
+    <meta property="og:title" content="Hello World!" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://glitch-hello-website.glitch.me/" />
+    <meta
+    property="og:description"
+    content="A simple website, built with Glitch. Remix it to get your own."
+    />
+    <meta
+    property="og:image"
+    content="https://cdn.glitch.com/b016c83f-6e10-47d9-91f9-025cc2acca25%2Fsjvxckjrvcukmtf066xr.jpg?v=1626833623012"
+    />
+    <meta name="twitter:card" content="summary" />
+
+    <!-- Import the webpage's stylesheet -->
+    <link rel="stylesheet" href="../style.css" />
+
+    <!-- Import the webpage's javascript file -->
+  </head>
+  <body>
+    <nav class="navbar bg-light pb-3 pt-3">
+      
+        <a
+            class="btn2--remix border-0 mx-auto"
+            target="_top"
+            href="/"
+        > Home</a>
+    
+        <a
+            class="btn2--remix mx-auto"
+            target="_top"
+            href="/discover"
+        > Discover</a>
+    
+        <a
+            class="btn2--remix border-0 mx-auto"
+            target="_top"
+            href="/connect"
+        > Connect</a>
+    
+        <a
+            class="btn2--remix border-0 mx-auto"
+            target="_top"
+            href="/profile"
+        > Profile</a>
+
+        <a
+            class="btn2--remix border-0 mx-auto"
+            target="_top"
+            href="/about.html"
+        > About</a>
+    
+        <a
+            class="btn2--remix border-0 mx-auto"
+            target="_top"
+            href="/resources"
+        > Resources</a>
+        <a
+            class="btn2--remix border-0 mx-auto"
+            target="_top"
+            href="/settings"
+        > Settings</a>
+      
+    </nav>  
+  
+    <div class="my-5 py-5">
+      <div class="content" role="main">
+
+        <h1 class="title">Discover</h1>
+        <h2>
+          Go above and beyond to see what you can create and learn.
+        </h2>
+        <span class="square"></span>
+      </div>
+    </div>
+
+    <hr/>
+
+    <div class="row container-fluid pt-3 ml-2">
+      <div class="col">
+        <h4 class="text-center"><b>Create</b></h4>
+
+        <p> Want to create your own game with ease? Launch our
+          studio without<br/> any downloads.
+        </p>
+      </div>
+
+      <div class="col-3 mt-4 pt-3 text-center">
+        <a
+          class="btn2--remix"
+          target="_top"
+          href="https://scratch.mit.edu/projects/editor/?tutorial=getStarted"
+          > Launch Our Studio 
+        </a> 
+      </div>
+    </div>
+
+    <div class="row container-fluid pt-3 ml-2">
+      <div class="col">
+        <h4 class="text-center"><b>Free For All</b></h4>
+        <p>
+          Don't want to create a game or watch any lessons? Try out Free For
+          All -- our own awesome built-in IDE. Code a game, experiment with
+          variables, or even just print Hello, World!
+        </p>
+      </div>
+      <div class="col-3 p-4 mt-4 ml-4 text-center">
+        <a
+          class="btn2--remix"
+          target="_top"
+          href="https://repl.it/languages/python3"
+        > CodeHaven IDE</a>
+      </div>
+    </div> 
+
+    <div class="row container-fluid pt-3 ml-2">
+      <div class="col">
+        <h4 class="text-center"><b>Lessons</b></h4>
+        <p>
+          Want to learn and go learn coding with ease?
+          Click on the button below to get started with lesson videos and
+          interactive quizzes!
+        </p>
+      </div>
+      <div class="col-3 mt-4 pt-3 text-center">
+        <a
+          class="btn2--remix"
+          target="_top"
+          href="https://code.org/curriculum/unplugged"
+      > Lessons</a>
+      </div>
+    </div>
+  
+    <div class="text-center mt-4 p-4" style="background-color: rgb(88, 111, 124);">
+      <p class="fw-bold" style="color:white; font-size:small;">Made for VMware's CodeHouse 2021</p>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"/>
+  </body>
+</html>
+</template>
+
+<script>
+
+
+</script>
+
+
+
+<style>
+    :root {
+  --color-bg: #FFFFFF;
+  --color-text-main: #000000;
+  --color-primary: #FFFF00;
+  --wrapper-height: 87vh;
+  --image-max-width: 300px;
+  --image-margin: 3rem;
+  --font-family: "HK Grotesk";
+  --font-family-header: "HK Grotesk";
+}
+
+/* Basic page style resets */
+* {
+  box-sizing: border-box;
+}
+[hidden] {
+  display: none !important;
+}
+
+/* Import fonts */
+@font-face {
+  font-family: HK Grotesk;
+  src: url("https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FHKGrotesk-Regular.otf?v=1603136326027")
+    format("opentype");
+}
+@font-face {
+  font-family: HK Grotesk;
+  font-weight: bold;
+  src: url("https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FHKGrotesk-Bold.otf?v=1603136323437")
+    format("opentype");
+}
+
+/* Our remix on glitch button */
+.btn--remix {
+  font-family: HK Grotesk;
+  padding: 0.75rem 1rem;
+  font-size: 1.1rem;
+  line-height: 1rem;
+  font-weight: 500;
+  height: 2.75rem;
+  align-items: center;
+  cursor: pointer;
+  background: #FFFFFF;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 4px;
+  text-decoration: none;
+  color: #000;
+  white-space: nowrap;
+  margin-left: auto;
+}
+.btn--remix img {
+  margin-right: 0.5rem;
+}
+.btn--remix:hover {
+  background-color: #D0FFF1;
+}
+.btn2--remix {
+  font-family: HK Grotesk;
+  padding: 0.75rem 1rem;
+  font-size: 1.1rem;
+  line-height: 1rem;
+  font-weight: 500;
+  height: 2.75rem;
+  align-items: center;
+  cursor: pointer;
+  background: #FFFFFF;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 4px;
+  text-decoration: none;
+  color: #000;
+  white-space: nowrap;
+  margin-left: auto;
+}
+.btn2--remix img {
+  margin-right: 0.5rem;
+}
+.btn2--remix:hover {
+  background-color: #D0FFF1;
+}
+
+
+.footer a:not(.btn2--remix):link,
+a:not(.btn--remix):visited {
+  font-family: HK Grotesk;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.1rem;
+  color: #000;
+  text-decoration: none;
+  border-style: none;
+}
+/* Navigation grid */
+.footer {
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem auto 0;
+  padding: 1rem 0 0.75rem 0;
+  width: 100%;
+  flex-wrap: wrap;
+  border-top: 4px solid #fff;
+}
+
+.footer a:not(.btn--remix):link,
+a:not(.btn--remix):visited {
+  font-family: HK Grotesk;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.1rem;
+  color: #000;
+  text-decoration: none;
+  border-style: none;
+}
+.footer a:hover {
+  background: var(--color-primary);
+}
+
+.footer .links {
+  padding: 0.5rem 1rem 1.5rem;
+  white-space: nowrap;
+}
+
+.divider {
+  padding: 0 1rem;
+}
+
+/******************************************************************************
+END Glitch hello-app default styles
+******************************************************************************/
+
+body {
+  font-family: HK Grotesk;
+  background-color: var(--color-bg);
+}
+
+/* Page structure */
+.wrapper {
+  min-height: var(--wrapper-height);
+  display: grid;
+  place-items: center;
+  margin: 0 1rem;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+/* Very light scaling for our illustration */
+.title {
+  color: #000000;
+  font-family: HK Grotesk;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 100px;
+  line-height: 105%;
+  margin: 0;
+}
+
+/* Very light scaling for our illustration */
+.illustration {
+  max-width: 100%;
+  max-height: var(--image-max-width);
+  margin-top: var(--image-margin);
+}
+
+/* Instructions */
+.instructions {
+  margin: 1rem auto 0;
+}
+
+/* Button - Add it from the README instructions */
+button,
+input {
+  font-family: inherit;
+  font-size: 100%;
+  background: #FFFFFF;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding: 0.5rem 1rem;
+  transition: 500ms;
+}
+
+/* Subheading */
+h2 {
+  color: #000000;
+}
+
+/* Interactive image */
+.illustration:active {
+  transform: translateY(5px);
+}
+
+/* Button dip
+- Toggling this class on and off will move it down and up again
+- The button transition property above determines the speed of the translate (500ms)
+*/
+.dipped {
+  transform: translateY(5px);
+}
+
+/*____________*/
+* {
+  margin: 1;
+  padding: 1;
+  font-family: san-serif;
+  background-size: cover;
+}
+
+body {
+  background-color: #c5d9ed;
+}
+
+.container {
+  margin-right: 200px;
+  margin-left: 200px;
+}
+
+/* Center logo */
+.logo {
+  width: 800px;
+}
+
+/* headers for each  */
+h1 {
+  text-align: center;
+  font-size: 45px;
+  background-color: #f2d675;
+  font: bold;
+  font-family: "Copperplate";
+}
+h2 {
+  color: #000000;
+  background-color: #f2d675;
+}
+
+p {
+  /*background-color: #f2d675;*/
+  font-size: 25px;
+  line-height: 30px;
+  
+  /*
+  Works for me
+  */
+}
+
+p1 {
+  font-size: 40px;
+  line-height: 40px;
+  color: white;
+  font: bold;
+  font-family: "Lucida Console";
+}
+
+.logo{
+  margin-top: 10px;
+  width:999px;
+}
+
+* {box-sizing:border-box}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Hide the images by default */
+.mySlides {
+  display: none;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
+}
+
+/* Make everything border-box*/
+
+div {
+  box-sizing: border-box;
+}
+
+</style>
